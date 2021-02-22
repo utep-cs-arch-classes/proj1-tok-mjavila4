caefb5148fab53e48ace447268fb270e5f5f3faf
Mark-Anthony <mjavila4@miners.utep.edu>
2021-02-22 08:53:42 -0700

    Added last three methods of milestone 2, word_end(), word_start() and word_count()

--- src/interface.c
+++ src/interface.c
@@ -13,6 +13,8 @@ void main(){
     input[i] = c;
   }
   input[i] = '\0';
+
+  printf("\n%d", count_words(input));
   
 }
 
@@ -28,4 +30,32 @@ int non_space_char(char c){
     return 0;
   }
   return 1;
+}
+
+char *word_start(char *str){
+  for(int i = 0; *(str+i) != '\0'; i++){
+    if(non_space_char(*(str+i))){
+      return str+i;
+    }
+  }
+  return 0;
+}
+
+char *word_end(char *str){
+  for(int i = 0; *(str+i) != '\0'; i++){
+    if(space_char(*(str+i))){
+      return str+i;
+    }
+  }
+  return 0;
+}
+
+int count_words(char *str){
+  int i = 0;
+  char *start;
+
+  for(i = 0; str && (start = word_start(str)) != 0; i++){
+    str = word_end(start);
+  }
+  return i;
 }
