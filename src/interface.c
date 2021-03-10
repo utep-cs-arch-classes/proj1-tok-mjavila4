8737a204632a7a01ba7b5004ecc82550e20c2207
Mark-Anthony <mjavila4@miners.utep.edu>
2021-03-09 17:17:28 -0700

    Added tokenize(), fixed sizeof() calls to allocate appropiate byte sizes

--- src/interface.c
+++ src/interface.c
@@ -8,17 +8,14 @@ void main(){
   
   int i;
   char c;
-  char *input = (char*)malloc(LIMIT);
+  char *input = (char*)malloc(sizeof(char*)*LIMIT);
 
   for(i = 0; (c = getchar()) != '\n' && i < LIMIT-1; i++){
     *(input+i) = c;
   }
-  
   *(input+99) = '\0';
 
-  printf("%s\n", input);
-
-  printf("%s\n", copy_str(input, 5));
+  printf(tokenize(input)[1]);
   
 }
 
@@ -57,7 +54,6 @@ char *word_end(char *str){
 int count_words(char *str){
   int i = 0;
   char *start;
-
   for(i = 0; str && (start = word_start(str)) != 0; i++){
     str = word_end(start);
   }
@@ -66,8 +62,7 @@ int count_words(char *str){
 
 char *copy_str(char *inStr, short len){
   int i;
-  char *copy = (char*)malloc(len+1);
-
+  char *copy = (char*)malloc(sizeof(char*)*len+1);
   for(i = 0; i<len; i++){
     *(copy+i) = *(inStr+i);
   }
@@ -75,7 +70,26 @@ char *copy_str(char *inStr, short len){
   return copy;
 }
 
+char **tokenize(char* str){
 
+  char **tokens = (char**)malloc(sizeof(char*)*count_words(str)+1);
+  char *start = word_start(str);
+  char *end = word_end(str);
+  int i,j;
+
+  for(i = 0; start != 0; i++){
+    end = word_end(start);
+
+    for(j = 0; start+j != end; j++){
+      if(*(start+j) == '\0'){break;}
+    }
+    tokens[i] = copy_str(start, j);
+    if(end == 0){break;}
+    start = word_start(end);
+  }
+  tokens[i+1] = 0;
+  return tokens;
+}
 
 
 
